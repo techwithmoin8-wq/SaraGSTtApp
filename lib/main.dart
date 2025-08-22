16fb68d226ce8cfb0b8a2d5d88051708704b0b60
techwithmoin8-wq <techwithmoin8@gmail.com>
2025-08-22 19:47:02 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class SaraApp extends StatelessWidget {
           foregroundColor: Colors.white,
           elevation: 0,
         ),
-        cardTheme: CardTheme(
+        cardTheme: CardThemedata(
           color: Colors.white,
           elevation: 2,
           shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
